c41b78b9adbde4e775ebf467914c7ba8d889387f
Fayecodeur <fayecodeur99@gmail.com>
2024-11-24 15:16:55 +0000

    finition de la page acceuil

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
       sm: "340px",
@@ -8,29 +8,30 @@ module.exports = {
       lg: "768px",
       xl: "1180px",
     },
-    extend: {},
-    keyframes: {
-      move: {
-        "50%": { transform: "translateY(-1rem)" },
+    extend: {
+      keyframes: {
+        move: {
+          "50%": { transform: "translateY(-1rem)" },
+        },
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        scaleUp: {
+          "0%": { transform: "scale(0.8)" },
+          "50%": { transform: "scale(1.2)" },
+          "100%": { transform: "scale(0.8)" },
+        },
       },
-      rotate: {
-        "0%": { transform: "rotate(0deg)" },
-        "100%": { transform: "rotate(360deg)" },
+      animation: {
+        movingY: "move 3s linear infinite",
+        rotating: "rotate 15s linear infinite",
+        scalingUp: "scaleUp 3s linear infinite",
       },
-      scaleUp: {
-        "0%": { transform: "scale(0.8)" },
-        "50%": { transform: "scale(1.2)" },
-        "100%": { transform: "scale(0.8)" },
+      fontFamily: {
+        Jost: ["Jost", "sans-serif"],
+        Lobster: ["Lobster", "sans-serif"],
       },
-    },
-    animation: {
-      movingY: "move 3s linear infinite",
-      rotating: "rotate 15s linear infinite",
-      scalingUp: "scaleUp 3s linear infinite",
-    },
-    fontFamily: {
-      Jost: ["Jost", "sans-serif"],
-      Lobster: ["Lobster", "sans-seri"],
     },
     container: {
       center: true,
